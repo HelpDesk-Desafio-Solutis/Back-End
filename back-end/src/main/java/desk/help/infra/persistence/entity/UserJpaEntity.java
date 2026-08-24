1b4fedb6aeacc79904a23b9b40d66e0ed83bf974
Enzo-Jorge <enzo.nascimento@sptech.school>
2026-08-24 14:09:32 -0300

    fix: Entidade JPA de usuário

--- back-end/src/main/java/desk/help/infra/persistence/entity/UserJpaEntity.java
+++ back-end/src/main/java/desk/help/infra/persistence/entity/UserJpaEntity.java
@@ -3,6 +3,7 @@ package desk.help.infra.persistence.entity;
 import desk.help.core.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,17 +14,20 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class UserJpaEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
+    @UuidGenerator
     private UUID uuid;
     private Boolean isActive;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private String name;
     private String email;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 
 }
